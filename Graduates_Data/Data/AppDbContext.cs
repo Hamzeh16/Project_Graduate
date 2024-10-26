66eb8ba5853577b4cd15d4f7354792477e0e4037
Hamzeh <Hamzeh@DESKTOP-28VKKV2>
2024-10-26 04:00:58 +0300

    Add project files.

---
+++ Graduates_Data/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Graduates_Model.Model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graduates_Data.Data
+{
+    public class AppDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        /// <summary>
+        /// Applicant User Table
+        /// </summary>
+        public DbSet<ApplicantUser> ApplicantsUser { get; set; }
+    }
+}
---
+++ Graduates_Model/Model/ApplicantUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graduates_Model.Model
+{
+    public class ApplicantUser : IdentityUser
+    {
+        [Required]
+        public int STUDENTID { get; set; }
+        [Required]
+        public string? STYDENTTYPE { get; set; }
+    }
+}
