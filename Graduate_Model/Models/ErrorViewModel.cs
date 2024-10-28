11529fbf8aac7ee3f37d79d98ba3d35478e43599
Hamzeh <Hamzeh@DESKTOP-28VKKV2>
2024-10-28 20:06:17 +0300

    delete file

--- Graduate_Model/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Graduate_Model.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
